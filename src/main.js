60e4b40382b26f9ff0339719f808caafb78071fe
William Calderipe <wcalder@thoughtworks.com>
2017-06-05 01:55:47 -0400

    Troca mensagem de hello

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <div>{'Hello, Webpack!'}</div>,
+  <div>{'Hello, React!'}</div>,
   document.getElementById('app')
 )
