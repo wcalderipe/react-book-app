095745de0a13ae23b680e5511579fd755dc29dab
William Calderipe <wcalder@thoughtworks.com>
2017-06-05 01:51:34 -0400

    Configura loader para JSX no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,16 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/main.js'),
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
