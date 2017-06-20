92a3c9ca4b55b70e26a992ed9ab79b5e564ca6ec
William Calderipe <wcalder@thoughtworks.com>
2017-06-20 02:14:21 -0300

    Cria o componente Header

---
+++ src/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+  render () {
+    return (
+      <header>Header Component</header>
+    ) 
+  }
+}
+
+export default Header
